741fac987377925623fbcfd4d9bf51ac1f6fe305
Carlos Daniel <dev.cdfaria@gmail.com>
2021-05-25 16:25:38 -0300

    Image directory

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ const DOM = {
       <td class="description">${transaction.description}</td>
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
-      <td><img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação"/></td>
+      <td><img onclick="Transaction.remove(${index})" src="./assets/images/minus.svg" alt="Remover transação"/></td>
     `
     return html
   },
